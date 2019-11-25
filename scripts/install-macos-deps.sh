bb66783f6239cd656303d681ff9ec63a063116ed
Luke Nelson <lukenels@cs.washington.edu>
2019-11-25 12:45:17 -0800

    Fix workflow

--- scripts/install-macos-deps.sh
+++ scripts/install-macos-deps.sh
@@ -1,23 +1,16 @@
 #!/usr/bin/env bash
 
 brew tap riscv/riscv
-brew install wget gcc llvm riscv-tools git
+brew install llvm riscv-tools
 
-case $RACKET_VERSION in
-    7.5-cs )
-        RACKET_URL="https://mirror.racket-lang.org/installers/7.5/racket-7.5-x86_64-macosx-cs.dmg"
+case $RACKET_VM in
+    cs )
+        brew cask install racket-cs
+        ;;
+    racket )
+        brew cask install racket
         ;;
 esac
-
-wget "$RACKET_URL"
-hdiutil attach $(basename "$RACKET_URL")
-cp -r "/Volumes/Racket*/Racket*" "/Applications"
-
-for name in $(ls /Applications/Racket*/bin); do
-    ln -sf "/Applications/Racket*/bin/${name}" "/usr/local/bin/${name}"
-done
-
-raco pkg install --auto -i rosette
 
 git clone 'https://github.com/emina/rosette.git'
 raco pkg install --auto -i --no-setup ./rosette && raco setup -Dl rosette
