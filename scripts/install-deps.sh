700131c513bf6e4def44d97085d3523b3887e4cb
Luke Nelson <lukenels@cs.washington.edu>
2020-03-27 13:25:16 -0700

    scripts: kill install-deps scripts
    I was testing these out for GitHub CI, but they're flakey compared to
    using the Docker image.

--- scripts/install-deps.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ $(uname) = 'Darwin' ]; then
-    ./scripts/install-macos-deps.sh
-elif [[ $(uname) = 'Linux' ]]; then
-    ./scripts/install-linux-deps.sh
-else
-    echo "Unknown OS"
-fi
