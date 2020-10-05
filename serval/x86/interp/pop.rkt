8b5f4a0a6886ecf0c1c290123aa4937c122504f2
Luke Nelson <lukenels@cs.washington.edu>
2020-10-05 14:17:28 -0700

    x86 interp: REX push and pop and leave

--- serval/x86/interp/pop.rkt
+++ serval/x86/interp/pop.rkt
@@ -4,19 +4,36 @@
   "common.rkt")
 
 (provide
-  pop-r)
+  pop-r pop-r64 leave)
 
+(define (interpret-pop cpu dst)
+  (define mm (cpu-memmgr cpu))
+  (define n (core:memmgr-bitwidth mm))
+  (define size (bv (quotient n 8) n))
+  (define sp (trunc n (cpu-gpr-ref cpu rsp)))
+  (define v (core:memmgr-load mm sp (bv 0 n) size #:dbg (cpu-pc-ref cpu)))
+  (cpu-gpr-set! cpu rsp (zero-extend (bvadd sp size) (bitvector 64)))
+  (cpu-gpr-set! cpu dst (zero-extend v (bitvector 64))))
 
 ; 58+rd
 (define-insn pop-r (dst)
   #:decode [((+r #x58 reg))
             (list (gpr64-no-rex reg))]
   #:encode (list (+r #x58 dst))
-  (lambda (cpu dst)
-    (define mm (cpu-memmgr cpu))
-    (define n (core:memmgr-bitwidth mm))
-    (define size (bv (quotient n 8) n))
-    (define sp (trunc n (cpu-gpr-ref cpu rsp)))
-    (define v (core:memmgr-load mm sp (bv 0 n) size #:dbg (cpu-pc-ref cpu)))
-    (cpu-gpr-set! cpu rsp (zero-extend (bvadd sp size) (bitvector 64)))
-    (cpu-gpr-set! cpu dst (zero-extend v (bitvector 64)))))
+  interpret-pop)
+
+; REX + 58+rd
+(define-insn pop-r64 (dst)
+  #:decode [((rex/r b) (+r #x58 reg))
+            (list (gpr64 b reg))]
+  #:encode (list (rex/r dst) (+r #x58 dst))
+  interpret-pop)
+
+; C9
+(define-insn leave ()
+  #:decode [((byte #xC9))
+            (leave)]
+  #:encode (list (byte #xC9))
+  (lambda (cpu)
+    (cpu-gpr-set! cpu rsp (cpu-gpr-ref cpu rbp))
+    (interpret-pop cpu rbp)))
--- serval/x86/interp/push.rkt
+++ serval/x86/interp/push.rkt
@@ -4,19 +4,27 @@
   "common.rkt")
 
 (provide
-  push-r)
+  push-r push-r64)
 
+(define (interpret-push cpu src)
+  (define mm (cpu-memmgr cpu))
+  (define n (core:memmgr-bitwidth mm))
+  (define size (bv (quotient n 8) n))
+  (define v (trunc n (cpu-gpr-ref cpu src)))
+  (define sp (bvsub (trunc n (cpu-gpr-ref cpu rsp)) size))
+  (cpu-gpr-set! cpu rsp (zero-extend sp (bitvector 64)))
+  (core:memmgr-store! mm sp (bv 0 n) v size #:dbg (cpu-pc-ref cpu)))
 
 ; 50+rd
 (define-insn push-r (src)
   #:decode [((+r #x50 reg))
             (list (gpr64-no-rex reg))]
   #:encode (list (+r #x50 src))
-  (lambda (cpu src)
-    (define mm (cpu-memmgr cpu))
-    (define n (core:memmgr-bitwidth mm))
-    (define size (bv (quotient n 8) n))
-    (define v (trunc n (cpu-gpr-ref cpu src)))
-    (define sp (bvsub (trunc n (cpu-gpr-ref cpu rsp)) size))
-    (cpu-gpr-set! cpu rsp (zero-extend sp (bitvector 64)))
-    (core:memmgr-store! mm sp (bv 0 n) v size #:dbg (cpu-pc-ref cpu))))
+  interpret-push)
+
+; REX + 50+rd
+(define-insn push-r64 (src)
+  #:decode [((rex/r b) (+r #x50 src))
+            (list (gpr64 b src))]
+  #:encode (list (rex/r src) (+r #x50 src))
+  interpret-push)
